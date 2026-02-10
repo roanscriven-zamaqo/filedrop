822e15cf32ff9eb0db9f2f2f8c3c3c997719d1ba
roanscriven-zamaqo <roan.scrivan@zamaqo.com>
2026-02-10 15:49:59 +0200

    Changes made to index.js and index.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+const fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readdir(process.env.PERSISTENT_STORAGE_DIR, function(err, files){
+    if (err) return res.sendStatus(500); 
+    files.sort(); 
+    return res.render('index', {title: "File Drop", files: files}); 
+  }); 
 });
 
 module.exports = router;
